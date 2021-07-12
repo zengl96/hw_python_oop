0c09f3e1c600dbced1a552dfcf2fc4c92579b4bd
кирилл <kirillefremov555@gmail.com>
2021-07-12 15:08:05 +0300

    'calculator'

--- homework.py
+++ homework.py
@@ -2,11 +2,22 @@ import datetime as dt
 class Calculator :
     def __init__(self,limit):
         self.limit= limit
-        self.record=[]
+        self.records=[]
     def add_record(self,record):
-        self.record.append(record)
-    def get_today_stats(self , Record):
-        pass
+        self.records.append(record)
+    def get_today_stats(self):
+        today = dt.date.today()
+        today1 = sum(record.amount for record in self.records
+                          if record.date == today)
+        return today1
+    def get_week_stats(self):
+        today = dt.date.today()
+        week_ago = today - dt.timedelta(7)
+        week_stats = sum(record.amount for record in self.records
+        if week_ago <= record.date <= today)
+        return week_stats
+    def jjj (self):
+        return  self.limit - self.get_today_stats()
 class Record :
     def __init__(self,amount,comment,date=None):
         self.amount=amount
@@ -15,4 +26,37 @@ class Record :
             self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
         else:
             self.date = dt.date.today()
-print('fdfdfd')
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):
+        calories = self.jjj()
+        if calories >= 0 :
+            return (f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {calories} кКал')
+        else:
+            return ('Хватит есть!')
+
+
+class CashCalculator(Calculator):
+    USD_RATE = 60.0
+    EURO_RATE = 70.0
+    RUB_RATE = 1.0
+
+    def get_today_cash_remained(self, currency):
+        cash_remained = self.jjj()
+        if cash_remained == 0:
+            return 'Денег нет, держись'
+        currencies = {
+            'eur': ('Euro', self.EURO_RATE),
+            'usd': ('USD', self.USD_RATE),
+            'rub': ('руб', self.RUB_RATE),
+        }
+        if currency not in currencies:
+            return 'No such currency.'
+        sign, rate = currencies.get(currency)
+        cash_remained = round(cash_remained / rate, 2)
+        if cash_remained > 0:
+            return f'На сегодня осталось {cash_remained} {sign}'
+        cash_remained = abs(cash_remained)
+        return f'Денег нет, держись: твой долг - {cash_remained} {sign}'
+
+
+
