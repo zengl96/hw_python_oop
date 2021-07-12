253d41599cb2c1a7a5dd890f917d9b0d937e9e32
кирилл <kirillefremov555@gmail.com>
2021-07-12 14:21:46 +0300

    ('fff')

--- homework.py
+++ homework.py
@@ -1,10 +1,12 @@
 import datetime as dt
-"""class Calculator :
+class Calculator :
     def __init__(self,limit):
         self.limit= limit
         self.record=[]
     def add_record(self,record):
-        self.record.append(record)"""
+        self.record.append(record)
+    def get_today_stats(self , Record):
+        pass
 class Record :
     def __init__(self,amount,comment,date=None):
         self.amount=amount
@@ -13,18 +15,4 @@ class Record :
             self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
         else:
             self.date = dt.date.today()
-    def r(self):
-        print(f"{r1.amount} {r1.comment} {self.date}")
-for i in range (0,3):
-    k1 = 100
-    a= int(input())
-    b= str(input())
-    r1 = Record(amount=a, comment=b)
-    r1.r()
-    k1 = k1-a
-    if k1>0:
-        print(f'Можешь тратить , у тебя осталось {k1}р ')
-    elif k1==0:
-        print(f'Не трать , у тебя осталось 0р ')
-    elif k1<0:
-        print(f'Можешь тратить , у тебя долг {k1}р ')
+print('fdfdfd')
